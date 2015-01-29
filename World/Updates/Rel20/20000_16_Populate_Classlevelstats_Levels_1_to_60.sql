f2f732157892b667f4b322d83981b6898bdcce49
Charles A Edwards <charles.a.edwards@btinternet.com>
2015-01-29 12:05:14 +0000

    Classlevelstats table populated
    table populated with data from Vanilla (levels 1 to 60, classes:
    Warrior, Paladin, and Mage).
    
    DB core updated as well as Updates files added.
    
    Prefix of previous file changed.

--- World/Updates/Rel20/20000_16_Populate_Classlevelstats_Levels_1_to_60.sql
+++ World/Updates/Rel20/20000_16_Populate_Classlevelstats_Levels_1_to_60.sql
@@ -1,60 +1,43 @@
---
--- Copyright (C) 2005-2014 MaNGOS <http://getmangos.eu/>
---
--- This program is free software; you can redistribute it and/or modify
--- it under the terms of the GNU General Public License as published by
--- the Free Software Foundation; either version 2 of the License, or
--- (at your option) any later version.
---
--- This program is distributed in the hope that it will be useful,
--- but WITHOUT ANY WARRANTY; without even the implied warranty of
--- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
--- GNU General Public License for more details.
---
--- You should have received a copy of the GNU General Public License
--- along with this program; if not, write to the Free Software
--- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
---
+-- --------------------------------------------------------------------------------
+-- This is an attempt to create a full transactional update
+-- --------------------------------------------------------------------------------
+DROP PROCEDURE IF EXISTS `update_mangos`; 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@SESSION.TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+DELIMITER $$
 
---
--- Table structure for table `creature_template_classlevelstats`
---
+CREATE DEFINER=`root`@`localhost` PROCEDURE `update_mangos`()
+BEGIN
+    DECLARE bRollback BOOL  DEFAULT FALSE ;
+    DECLARE CONTINUE HANDLER FOR SQLEXCEPTION SET `bRollback` = TRUE;
 
-DROP TABLE IF EXISTS `creature_template_classlevelstats`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `creature_template_classlevelstats` (
-  `Level` tinyint(4) NOT NULL,
-  `Class` tinyint(4) NOT NULL,
-  `BaseHealthExp0` mediumint(8) unsigned NOT NULL DEFAULT '0',
-  `BaseHealthExp1` mediumint(8) unsigned NOT NULL DEFAULT '0',
-  `BaseMana` mediumint(8) unsigned NOT NULL DEFAULT '0',
-  `BaseDamageExp0` float NOT NULL DEFAULT '0',
-  `BaseDamageExp1` float NOT NULL DEFAULT '0',
-  `BaseMeleeAttackPower` float NOT NULL DEFAULT '0',
-  `BaseRangedAttackPower` float NOT NULL DEFAULT '0',
-  `BaseArmor` mediumint(8) unsigned NOT NULL DEFAULT '0',
-  PRIMARY KEY (`Level`,`Class`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  SET @cOldRev = 'required_20000_15_Startup_errors_Cleanup2'; 
 
---
--- Dumping data for table `creature_template_classlevelstats`
---
+  -- Set the new revision string
+  SET @cNewRev = 'required_20000_16_Populate_Classlevelstats_Levels_1_to_60';
 
-LOCK TABLES `creature_template_classlevelstats` WRITE;
-/*!40000 ALTER TABLE `creature_template_classlevelstats` DISABLE KEYS */;
+  -- Set thisRevision to the column name of db_version in the currently selected database
+  SET @cThisRev := ((SELECT column_name FROM information_schema.`COLUMNS` WHERE table_name='db_version' AND table_schema=(SELECT DATABASE() AS thisDB FROM DUAL) AND column_name LIKE 'required%'));
+
+ 
+  -- Only Proceed if the old values match
+  IF @cThisRev = @cOldRev THEN
+    -- Make this all a single transaction
+    START TRANSACTION;
+
+    -- Apply the Version Change from Old Version to New Version
+    SET @query = CONCAT('ALTER TABLE db_version CHANGE COLUMN ',@cOldRev, ' ' ,@cNewRev,' bit;');
+    PREPARE stmt1 FROM @query;
+    EXECUTE stmt1;
+    DEALLOCATE PREPARE stmt1;
+    -- The Above block is required for making table changes
+
+    -- -- -- -- Normal Update / Insert / Delete statements will go here  -- -- -- -- --
+    
+    
+    -- Populate the creature_template_classlevelstats table with data for Warrior, Paladin, and Mage, level 1 to 60
+    -- Data taken from the Vanilla database
+    
+DELETE FROM creature_template_classlevelstats WHERE Class IN (1, 2, 8);
 INSERT INTO `creature_template_classlevelstats` (`Level`, `Class`, `BaseHealthExp0`, `BaseMana`, `BaseDamageExp0`, `BaseMeleeAttackPower`, `BaseRangedAttackPower`, `BaseArmor`) VALUES (1,1,42,0,0.0607,10,1,8),
 (2,1,55,0,0.3603,10,1,20),
 (3,1,71,0,0.6976,11,1,33),
@@ -244,16 +227,29 @@ INSERT INTO `creature_template_classlevelstats` (`Level`, `Class`, `BaseHealthEx
 (61,8,2201,5875,29.2976,115,25,3263),
 (62,8,2266,6015,29.7969,116,25,3500),
 (63,8,2332,6156,30.2962,119,26,3736);
-/*!40000 ALTER TABLE `creature_template_classlevelstats` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+    
+    
+    
+    -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+    
+    -- If we get here ok, commit the changes
+    IF bRollback = TRUE THEN
+      ROLLBACK;
+      SELECT '* UPDATE FAILED *' AS 'Status',@cThisRev AS 'DB is on Version';
+    ELSE
+      COMMIT;
+      SELECT '* UPDATE COMPLETE *' AS 'Status',@cNewRev AS 'DB is now on Version';
+    END IF;
+  ELSE
+    SELECT '* UPDATE SKIPPED *' AS 'Status',@cOldRev AS 'Required Version',@cThisRev AS 'Found Version';
+  END IF;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+END $$
 
--- Dump completed
+DELIMITER ;
+
+-- Execute the procedure
+CALL update_mangos();
+
+-- Drop the procedure
+DROP PROCEDURE IF EXISTS `update_mangos`;
