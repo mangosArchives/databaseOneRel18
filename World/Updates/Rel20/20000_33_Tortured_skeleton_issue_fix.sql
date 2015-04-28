37af927b3fd45f2bac95c23892fe4649bed76b18
SylvainNeau <syneau@gmail.com>
2015-04-28 06:53:57 +0200

    From Chuck5ta advises
    Ajout d'un DELETE
    Suppression référence à mangos1

--- World/Updates/Rel20/20000_33_Tortured_skeleton_issue_fix.sql
+++ World/Updates/Rel20/20000_33_Tortured_skeleton_issue_fix.sql
@@ -41,10 +41,11 @@ BEGIN
 	
 	-- Use of an EnventAI
 	UPDATE creature_template SET MechanicImmuneMask = 1  WHERE entry = 17916;
+	DELETE FROM creature_ai_scripts where id = 1879701 AND creature_id = 18797;
 	INSERT INTO creature_ai_scripts (id,creature_id,event_type,event_inverse_phase_mask,event_chance,event_flags,event_param1,event_param2,event_param3,event_param4,action1_type,action1_param1,action1_param2,action1_param3,action2_type,action2_param1,action2_param2,action2_param3,action3_type,action3_param1,action3_param2,action3_param3,comment)
 	VALUES
 	(1879701,18797,4,0,100,6,0,0,0,0,47,0,0,0,0,0,0,0,0,0,0,0,'Tortured skeleton - stand on aggro');
-	UPDATE mangos1.creature_template SET AIName = 'EventAI' WHERE Entry = 18797;
+	UPDATE creature_template SET AIName = 'EventAI' WHERE Entry = 18797;
     -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
     
     -- If we get here ok, commit the changes
